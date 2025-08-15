836b894ca7ea8c70ced1d75f72954013c29617b2
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-15 16:36:21 -0300

    feat: a tipagem e operacoes matematicas

---
+++ Exercicios/calculos.py
@@ -0,0 +1,15 @@
+import os
+os.system("cls")
+
+primeiro_numero = float(input("Digite o primeiro número: "))
+segundo_numero = float(input("Digite o segundo número: "))
+
+soma = primeiro_numero + segundo_numero
+subtracao = primeiro_numero - segundo_numero
+multiplicacao = primeiro_numero * segundo_numero
+divisao = primeiro_numero / segundo_numero
+
+print(f"O resultado da soma é: {soma}")
+print(f"O resultado da subtração é: {subtracao}")
+print(f"O resultado da multiplicação é: {multiplicacao}")
+print(f"O resltado da divisão é: {divisao}")
---
+++ tipagem.py
@@ -0,0 +1,11 @@
+# Tipagem dinâmica.
+
+# Limpar o terminal se o código for executado no terminal.
+import os
+os.system("cls")
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
