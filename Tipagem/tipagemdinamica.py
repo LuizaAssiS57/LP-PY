79ffaf447fae53583b11166745b2dd60b645b1db
LuizaAssiS57 <luizaassis57@gmail.com>
2025-08-21 18:25:04 -0300

    feat: Criando a pasta de tipagem

---
+++ Tipagem/tipagemdinamica.py
@@ -0,0 +1,9 @@
+#Tipagem dinâmica.
+
+nome = "Marta"
+idade = 25
+altura = 1.89
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Altura: {altura}")
---
+++ Tipagem/tipagemestatica.py
@@ -0,0 +1,13 @@
+#Tipagem estática.
+
+nome : str
+idade : int
+altura : float
+
+nome = "Marta"
+idade = 25
+altura = 1.78
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Altura: {altura}")
